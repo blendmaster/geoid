80f6757954aa68d7134a420af7330bf6eb8f68ea
Steven Ruppert <blendmaster@gmail.com>
2013-04-16 14:04:51 -0600

    Remove fullscreen titlebar hiding stuff
    From initial scaffold.

--- src/g/oid/GeoidActivity.java
+++ src/g/oid/GeoidActivity.java
@@ -19,175 +19,39 @@ import org.opencv.features2d.Features2d;
 import org.opencv.features2d.KeyPoint;
 import org.opencv.imgproc.Imgproc;
 
-import g.oid.util.SystemUiHider;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- * 
- * @see SystemUiHider
+ * You've got the whole wide augmented world, in your augmented hands.
  */
 public class GeoidActivity extends Activity implements CvCameraViewListener2 {
-	/**
-	 * Whether or not the system UI should be auto-hidden after
-	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-	 */
-	private static final boolean AUTO_HIDE = true;
-
-	/**
-	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-	 * user interaction before hiding the system UI.
-	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-	/**
-	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
-	 * will show the system UI visibility upon interaction.
-	 */
-	private static final boolean TOGGLE_ON_CLICK = true;
-
-	/**
-	 * The flags to pass to {@link SystemUiHider#getInstance}.
-	 */
-	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-	/**
-	 * The instance of the {@link SystemUiHider} for this activity.
-	 */
-	private SystemUiHider mSystemUiHider;
-
 	private CameraBridgeViewBase cameraView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		setContentView(R.layout.activity_geoid);
 
-		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.camera_preview);
-
-		// Set up an instance of SystemUiHider to control the system UI for
-		// this activity.
-		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
-				HIDER_FLAGS);
-		mSystemUiHider.setup();
-		mSystemUiHider
-				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-					// Cached values.
-					int mControlsHeight;
-					int mShortAnimTime;
-
-					@Override
-					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-					public void onVisibilityChange(boolean visible) {
-						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-							// If the ViewPropertyAnimator API is available
-							// (Honeycomb MR2 and later), use it to animate the
-							// in-layout UI controls at the bottom of the
-							// screen.
-							if (mControlsHeight == 0) {
-								mControlsHeight = controlsView.getHeight();
-							}
-							if (mShortAnimTime == 0) {
-								mShortAnimTime = getResources().getInteger(
-										android.R.integer.config_shortAnimTime);
-							}
-							controlsView
-									.animate()
-									.translationY(visible ? 0 : mControlsHeight)
-									.setDuration(mShortAnimTime);
-						} else {
-							// If the ViewPropertyAnimator APIs aren't
-							// available, simply show or hide the in-layout UI
-							// controls.
-							controlsView.setVisibility(visible ? View.VISIBLE
-									: View.GONE);
-						}
-
-						if (visible && AUTO_HIDE) {
-							// Schedule a hide().
-							delayedHide(AUTO_HIDE_DELAY_MILLIS);
-						}
-					}
-				});
-
-		// Set up the user interaction to manually show or hide the system UI.
-		contentView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (TOGGLE_ON_CLICK) {
-					mSystemUiHider.toggle();
-				} else {
-					mSystemUiHider.show();
-				}
-			}
-		});
-
-		// Upon interacting with UI controls, delay any scheduled hide()
-		// operations to prevent the jarring behavior of controls going away
-		// while interacting with the UI.
-		// findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
 		cameraView = (CameraBridgeViewBase) contentView;
 		cameraView.setVisibility(SurfaceView.VISIBLE);
 
 		cameraView.setCvCameraViewListener(this);
 	}
-
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		// Trigger the initial hide() shortly after the activity has been
-		// created, to briefly hint to the user that UI controls
-		// are available.
-		delayedHide(100);
-	}
-
-	/**
-	 * Touch listener to use for in-layout UI controls to delay hiding the
-	 * system UI. This is to prevent the jarring behavior of controls going away
-	 * while interacting with activity UI.
-	 */
-	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			if (AUTO_HIDE) {
-				delayedHide(AUTO_HIDE_DELAY_MILLIS);
-			}
-			return false;
-		}
-	};
-
-	Handler mHideHandler = new Handler();
-	Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			mSystemUiHider.hide();
-		}
-	};
-
-	/**
-	 * Schedules a call to hide() in [delay] milliseconds, canceling any
-	 * previously scheduled calls.
-	 */
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
-	}
-
+	
 	@Override
 	protected void onPause() {
 		super.onPause();
@@ -260,10 +124,6 @@ public class GeoidActivity extends Activity implements CvCameraViewListener2 {
 	// the current set of descriptors, and the set of descriptors to match
 	private Mat queryDescriptors, trainDescriptors;
 
-	// saved image/keypoints that descriptors were pulled from
-	private Mat trainingImage;
-	private MatOfKeyPoint trainingKeypoints;
-
 	// these Mats are declared as members and reused to prevent new allocations
 	// every frame, which android's garbage detector is bad at handling
 	private Mat rgb, output, input;
@@ -272,13 +132,10 @@ public class GeoidActivity extends Activity implements CvCameraViewListener2 {
 		detector = FeatureDetector.create(FeatureDetector.ORB);
 		extractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
 		matcher = DescriptorMatcher
-				.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+				.create(DescriptorMatcher.BRUTEFORCE_HAMMINGLUT);
 
 		rgb = new Mat();
 		output = new Mat();
-
-		trainingImage = new Mat();
-		trainingKeypoints = new MatOfKeyPoint();
 
 		queryDescriptors = new Mat();
 		trainDescriptors = new Mat();
@@ -302,9 +159,6 @@ public class GeoidActivity extends Activity implements CvCameraViewListener2 {
 		if (grabFeatures) {
 			Log.d("geoid", "grabbing features from current frame...");
 			extractor.compute(rgb, keypoints, trainDescriptors);
-
-			// trainingKeypoints = keypoints;
-			trainingImage.setTo(rgb);
 
 			grabFeatures = false;
 			trained = true;
@@ -333,7 +187,7 @@ public class GeoidActivity extends Activity implements CvCameraViewListener2 {
 				}
 			}
 
-			float threshold = 2 * min;
+			float threshold = (float) (1.2 * min);
 
 			// draw good, matching keypoints
 			KeyPoint[] points = keypoints.toArray();
