2592b6a46323c2f8d47bc2e3e80990c6f4d60a86
Steven Ruppert <blendmaster@gmail.com>
2013-04-20 19:02:48 -0600

    Implement surface_point
    I think

--- simulation/sim.py
+++ simulation/sim.py
@@ -3,18 +3,57 @@
 # In real life, the points could be LEDs on the globe's surface for easy
 # detection using thresholding and region centroids.
 import cv2
+import numpy as np
+
+radius = 1
+
+def rot_z(angle):
+  cost = np.cos(np.deg2rad(angle))
+  sint = np.sin(np.deg2rad(angle))
+  return np.array([[cost, -sint, 0],
+                   [sint, cost, 0],
+                   [0, 0, 1]])
+
+def rot_y(angle):
+  cost = np.cos(np.deg2rad(angle))
+  sint = np.sin(np.deg2rad(angle))
+  return np.array([[cost, 0, sint],
+                   [0, 1, 0 ],
+                   [-sint, 0, cost]])
+
+def rot_x(angle):
+  cost = np.cos(np.deg2rad(angle))
+  sint = np.sin(np.deg2rad(angle))
+  return np.array([[1, 0, 0],
+                   [0, cost, -sint],
+                   [0, sint, cost]])
 
 def surface_point(lat, lon, globe_pose):
   """The 3D coordinates of a latitude/longitude surface point on the globe."""
+  x_g, y_g, z_g, xrot, yrot, zrot = globe_pose
 
-  return stuff
+  y = np.sin(np.deg2rad(lat))
+
+  y_radius = np.cos(np.deg2rad(lat))
+
+  x = y_radius * np.cos(np.deg2rad(lon))
+  z = y_radius * np.sin(np.deg2rad(lon))
+
+  # rotate
+  rot = np.dot(rot_z(zrot), np.dot(rot_y(yrot), rot_x(xrot)))
+  transformed = np.dot(rot, np.array([[x], [y], [z]]))
+
+  [[x], [y], [z]] = transformed
+
+  # translate
+  return (x + x_g, y + y_g, z + z_g)
 
 def simulated_image(points, camera_intrinsics, camera_extrinsics, globe_pose):
-  """From a camera pose and globe pose in world coordinates, a binary image
+  """From a camera pose and globe pose in world coordinates, an image
   containing the projected points on the globe, with a bit of noise and
   extraneous parts."""
 
-  img = cv.Mat()
+  img = np.zeros((640, 480, 3), np.uint8)
 
   projection_matrix = stuff
 
@@ -27,10 +66,10 @@ def simulated_image(points, camera_intrinsics, camera_extrinsics, globe_pose):
     # draw on image
     cv.Circle(img, center, etc)
 
-  for some amount of randomness:
-    add random blotches
+  #for some amount of randomness:
+    #add random blotches
 
-  add noise to img
+  #add noise to img
 
   return img
 
@@ -76,14 +115,14 @@ height = 480
 
 camera_fov = 66.41 # of the rear camera, in degrees, across the diagonal
 
-camera_intrinsics = stuff
+#camera_intrinsics = stuff
 
-# rotation, translation
-camera_extrinsics = stuff
+## rotation, translation
+#camera_extrinsics = stuff
 
-globe_pose = stuff
+#globe_pose = stuff
 
-# points on a globe by latitude and longitude
-points = stuff
+## points on a globe by latitude and longitude
+#points = stuff
 
-simulate(camera_intrinscs, camera_extrinsics, globe_pose, points)
+#simulate(camera_intrinscs, camera_extrinsics, globe_pose, points)
