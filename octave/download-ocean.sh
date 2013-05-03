169cd92ca96a7f3c6448972ddb770f450c3de577
Steven Ruppert <blendmaster@gmail.com>
2013-05-02 22:49:10 -0600

    Add ocean data download script
    What a wonderful world of dataset access

---
+++ octave/download-ocean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# a simple curl invocation to grab the ECCO2 ocean dataset from OPeNDAP at
+# http://ecco2.jpl.nasa.gov/opendap/data1/cube/cube92/lat_lon/quart_90S_90N/
+
+# we only want the first depth layer, which saves a ton of space. it's really
+# nice that ndap allows subselections.
+
+curl \
+  "http://ecco2.jpl.nasa.gov:80/opendap/data1/cube/cube92/lat_lon/quart_90S_90N/UVEL_monthly.nc/UVEL.1440x720x50.[2011-2012][01-12].nc.nc?UVEL\[0\]\[0\]\[0:1:719\]\[0:1:1439\]"\
+  -o "uvel-#1#2.nc"
+
+curl \
+  "http://ecco2.jpl.nasa.gov:80/opendap/data1/cube/cube92/lat_lon/quart_90S_90N/VVEL_monthly.nc/VVEL.1440x720x50.[2011-2012][01-12].nc.nc?VVEL\[0\]\[0\]\[0:1:719\]\[0:1:1439\]"\
+  -o "vvel-#1#2.nc"
